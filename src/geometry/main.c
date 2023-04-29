1dd3bd57f3591fcf27bc282bdd44bde75bd9c1f7
Anton Dmitriev <tony19dm@PC-Anton>
2023-04-29 10:18:32 +0700

    main.c changed

--- src/geometry/main.c
+++ src/geometry/main.c
@@ -1,28 +1,47 @@
-#include <libgeometry/area.h>
-#include <libgeometry/checkarg.h>
-#include <libgeometry/perimetr.h>
-#include <libgeometry/vocabulary.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <libgeometry/intersection.h>
+#include <libgeometry/memory.h>
+#include <libgeometry/output.h>
 
 #define SIZE 100
 
 int main()
 {
-    FILE* file;
-    file = fopen("testdata.txt", "r");
-    if (file == NULL) {
+    FILE *file;
+    file = fopen("tesedata.txt", "r");
+    if (file == NULL)
+    {
         printf("Error of oppening file!");
         return 1;
     }
-    char str1[SIZE];
-    int countFigures = 0;
-    while (fgets(str1, SIZE, file)) {
-        countFigures++;
-        Errors(str1, countFigures);
+    char *line = (char *)malloc(SIZE * sizeof(char));
+    char **lines = (char **)malloc(sizeof(char *));
+    int row = 1, num = 1;
+    while (fgets(line, SIZE, file))
+    {
+        int check = isCircle(line);
+        if (!check)
+        {
+            lines = (char **)realloc(lines, row * sizeof(char *));
+            lines[row - 1] = (char *)malloc(strlen(line) * sizeof(char));
+            strcpy(lines[row - 1], line);
+            row++;
+        }
+        else
+        {
+            printError(line, check, num++);
+        }
     }
-
     fclose(file);
+
+    printf("\n\nCorrect figures in WKT format:");
+    intersections(lines, row - 1);
+
+    freeMemForStr(lines, row - 1);
+    free(line);
     printf("\n");
     return 0;
 }
